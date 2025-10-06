766077992db0b6b1a242d0adef74f3a6a4f32a64
NataPonni <ponni@ukr.net>
2025-10-06 14:01:21 +0200

    Modify app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory
+from flask import request, jsonify
 from flask_cors import CORS
 import sqlite3, hashlib
 
@@ -54,5 +55,12 @@ def login():
     else:
         return jsonify({"error": "Invalid credentials"}), 401
 
-if __name__ == "__main__":
+app = Flask(__name__, static_folder='../frontend', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'startsite.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
+
